8e7e788aadd087655cf96b84572a63a83381ff16
Abdukarim <abdukarimt463@gmail.com>
2026-03-25 23:13:08 +0500

    navbar yaratildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Navbar from "./assets/components/navbar/Navbar"
 
 function App() {
   return (
-    <div>App</div>
+    <div className="bg-slate-900 w-full min-h-screen h-auto text-white font-mono mx-auto " >
+      <div className="container px-20 py-10 border mx-auto " >
+        <Navbar />
+      </div>
+    </div>
   )
 }
 
---
+++ src/assets/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+    return (
+        <nav className="flex cursor-pointer *:cursor-pointer items-center justify-between flex-wrap bg-teal-500 p-7 rounded-lg">
+
+        </nav>
+    )
+}
+
+export default Navbar
